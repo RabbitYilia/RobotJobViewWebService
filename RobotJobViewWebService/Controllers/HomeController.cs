421a2f9ae824ef3548347a1641f00985324ef94c
Rabbit Yilia <takatsuki@outlook.com>
2018-09-02 20:43:28 +0800

    Ready Upload

--- RobotJobViewWebService/Controllers/HomeController.cs
+++ RobotJobViewWebService/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace RobotJobViewWebService.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "This is a demo for preview";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Author contact page.";
 
             return View();
         }
